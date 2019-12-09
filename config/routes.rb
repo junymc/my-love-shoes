680239b7624a4db07a4ffeef559f43bd969ba40f
github username <github email address>
2019-12-09 15:13:41 -0800

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resourses :brands
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resourses :shoes
+    end
+  end
+
 end
