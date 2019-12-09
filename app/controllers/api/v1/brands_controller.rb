8986d6127ef6ffb35c16cde75ced2435558f50dc
github username <github email address>
2019-12-09 12:42:51 -0800

    adding grud actions in the brands_controller

--- app/controllers/api/v1/brands_controller.rb
+++ app/controllers/api/v1/brands_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::BrandsController < ApplicationController
+    def index
+        @brands = Brand.all
+
+        render json: @brands, status: 200
+    end
+
+    def show
+        @brand = Brand.find(params[:id])
+
+        render json: @brand, status: 200
+    end
+
+    def create
+        @brand = Brand.create(brand_params)
+
+        render json: @brand, status: 200
+    end
+
+    def update
+        @brand = Brand.create(params[:id])
+        @brand.update(brand_params)
+        render json: @brand, status: 200
+    end
+
+    def destroy
+        @brand = Brand.find(params[:id])
+        @brand.delete
+
+        render json: {brandId: @brand.id}
+    end
+
+    private
+    def brand_params
+        params.require(:brand).permit(:body)
+    end
+
 end
--- app/controllers/api/v1/shoes_controller.rb
+++ app/controllers/api/v1/shoes_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::ShoesController < ApplicationController
+    def index
+        @shoes = Shoe.all
+
+        render json: @shoes, status: 200
+    end
 end
