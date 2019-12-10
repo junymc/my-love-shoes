13959aea6c1a02feffe005ad9d5f58b047a67bc1
github username <github email address>
2019-12-09 16:08:35 -0800

    creating migrations

--- db/migrate/20191209231804_create_brands.rb
+++ db/migrate/20191209231804_create_brands.rb
@@ -1,6 +1,7 @@
 class CreateBrands < ActiveRecord::Migration[6.0]
   def change
     create_table :brands do |t|
+      t.string :name
 
       t.timestamps
     end
--- db/migrate/20191209231902_create_shoes.rb
+++ db/migrate/20191209231902_create_shoes.rb
@@ -1,6 +1,10 @@
 class CreateShoes < ActiveRecord::Migration[6.0]
   def change
     create_table :shoes do |t|
+      t.string :model
+      t.integer :size
+      t.string :color
+      t.string :type
 
       t.timestamps
     end
